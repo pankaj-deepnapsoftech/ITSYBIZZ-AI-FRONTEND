68a1be1ff3a353ffaa8a83902708ae57e543d329
pankaj-deepnapsoftech <itsybizz@gmail.com>
2026-01-20 18:06:30 +0530

    updatess

--- src/App.jsx
+++ src/App.jsx
@@ -226,7 +226,7 @@ const Login = lazy(() => import("./Pages/Login"));
 const PrivacyPolicy = lazy(() => import("./Components/Legal/PrivacyPolicy"));
 const Term = lazy(() => import("./Components/Legal/Term"));
 const DigitalMarketing = lazy(() => import("./Pages/DigitalMarketing"));
-const MetaAds = lazy(() => import("./Components/DigitalMarketing/MetaAds"));
+const MetaAds = lazy(() => import("./Components/digitalMarketing/MetaAds"));
 const Contact = lazy(() => import("./Pages/Contact"));
 const Career = lazy(() => import("./Components/Career"));
 const Dashboard = lazy(() => import("./Pages/Admin/Dashboard"));
@@ -327,7 +327,7 @@ const App = () => {
             <Route path="about-us" element={<About />} />
             <Route path="policy" element={<PrivacyPolicy />} />
             <Route path="terms" element={<Term />} />
-            <Route path="meta/ads" element={<MetaAdds />} />
+            <Route path="meta/ads" element={<MetaAds />} />
             <Route path="contact-us" element={<Contact />} />
             <Route path="career" element={<Career />} />
             <Route path="Iot-products" element={<IotProducts />} />
